af267f3a263089918ca9baa63263eac6ad88c3d5
Julien Brasseur <julien.brasseur@dwarf-labs.com>
2015-05-18 13:58:10 +0200

    WIP share intent

--- app/src/main/java/narthe/compteur_km/ExportActivity.java
+++ app/src/main/java/narthe/compteur_km/ExportActivity.java
@@ -1,25 +1,27 @@
 package narthe.compteur_km;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-import org.joda.time.DateTime;
+import java.io.File;
 
 
 public class ExportActivity extends ActionBarActivity {
 
     Button exportButton;
+    File pdf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
+        this.initWidgets();
+        this.initEvents();
     }
 
 
@@ -39,10 +41,17 @@ public class ExportActivity extends ActionBarActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        startActivity(Intent.createChooser(new Intent(createShareIntent(pdf)), getString(R.string.sendvia)));
                     }
                 }
         );
+    }
+
+    private Intent createShareIntent(File f) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("application/pdf");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, f);
+        return shareIntent;
     }
 
     @Override
